02481fbc1e00dccbc703fc1fd8fc4b420ca73425
Gabriel Rissi <gbrissi123@gmail.com>
2022-08-22 22:08:37 -0300

    localhost api finished

--- db/models/user.js
+++ db/models/user.js
@@ -4,6 +4,19 @@ class User extends Model {
     static get tableName() {
         return 'user';
     }
+    static get relationMappings() {
+        const Channel = require('./channel')
+        return {
+            channel: {
+                relation: Model.HasOneRelation,
+                modelClass: Channel,
+                join: {
+                    from: 'user.channelId',
+                    to: 'channel.id'
+                }
+            }
+        }
+    }
 }
 
 module.exports = User;
